69c6afe9e65490942cb08cb6f97603ea82d9a3e2
jp2475 <jp2475@cornell.edu>
2022-02-16 19:01:05 -0500

    small test

--- tablecodec/tablecodec.go
+++ tablecodec/tablecodec.go
@@ -86,7 +86,7 @@ func DecodeRecordKey(key kv.Key) (tableID int64, handle int64, err error) {
 		err = errInvalidIndexKey.GenWithStack("invalid index key - %q %v", key, err)
 		return 0, 0, err
 	}
-	return tableID, handle, nil
+	return tableID, handle, err
 }
 
 // appendTableIndexPrefix appends table index prefix  "t[tableID]_i".
